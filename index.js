4569a219d8df70403e8ec3eff7833d118cb8f70b
Harry Shamansky <harry.shamansky@gmail.com>
2020-06-25 23:39:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,57 +7,55 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
+  homebridge.registerAccessory("homebridge-index", "IndexCounter", IndexCounter);
 }
 
-function DummySwitch(log, config) {
+function IndexCounter(log, config) {
   this.log = log;
   this.name = config.name;
-  this.stateful = config.stateful;
-  this.reverse = config.reverse;
-  this.time = config.time ? config.time : 1000;		
-  this._service = new Service.Switch(this.name);
+  this.max = config.max;
+  this.time = config.time;
+  this._service = new Service.AccessoryInformation(this.name);
   
   this.cacheDirectory = HomebridgeAPI.user.persistPath();
   this.storage = require('node-persist');
   this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
   
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
-
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
-
-  if (this.stateful) {
-	var cachedState = this.storage.getItemSync(this.name);
-	if((cachedState === undefined) || (cachedState === false)) {
-		this._service.setCharacteristic(Characteristic.On, false);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-	}
-  }
+  let indexCustomCharacteristic = new Index(config.max);
+	
+  this._service.addCharacteristic(indexCustomCharacteristic);
+  indexCustomCharacteristic.on('set', this._setIndex.bind(this));
 }
 
-DummySwitch.prototype.getServices = function() {
+IndexCounter.prototype.getServices = function() {
   return [this._service];
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
+IndexCounter.prototype._setIndex = function(index, callback) {
 
-  this.log("Setting switch to " + on);
+  this.log("Setting index to " + index);
+  this.storage.setItemSync(this.name, index);
+  setTimeout(function() {
+    let newRand = Math.floor(Math.random() * (this.max + 1));
+    let indexCustomCharacteristic = new Index(config.max);
+    let indexCharacteristic = this._service.getCharacteristic(indexCustomCharacteristic);
+    this._service.setCharacteristic(indexCharacteristic, newRand);
+  }.bind(this), this.time);
 
-  if (on && !this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), this.time);
-  } else if (!on && this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, true);
-    }.bind(this), this.time);
-  }
-  
-  if (this.stateful) {
-	this.storage.setItemSync(this.name, on);
-  }
-  
   callback();
+}
+	
+class Index extends Characteristic {
+
+  constructor(max) {
+    super('Index', '000000CE-0000-1000-8000-0026ABCDEF01');
+    this.setProps({
+      format: Formats.UINT32,
+      maxValue: max,
+      minValue: 0,
+      minStep: 1,
+      perms: [Perms.READ, Perms.WRITE, Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  }
 }
